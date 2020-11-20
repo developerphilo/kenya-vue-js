7cb4c95bf1a210e64fbb23a5c04cbf174d277802
John Philip <developerphilo@gmail.com>
2020-11-20 09:34:13 +0300

    updated Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <p>Home Route Component</p>
+      <p>This is the vue router home component<br> It deeply integrates with Vue.js core to make building <br> Single Page Applications with Vue.js a breeze.</p>
   </div>
 </template>
 
@@ -9,9 +10,10 @@
 <style scoped>
 p{
 
-  padding-top: 20px;
+  padding-top: 40px;
   text-align: center;
   font-size: 20px;
+  line-height: 2.2rem;
 }
 
 </style>
